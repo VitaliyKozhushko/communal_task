b9dd1aab0b636f965c3fc8a1c2a0e796cb9df6a6
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-07 10:08:36 +0300

    created app home

--- communal_task/settings.py
+++ communal_task/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
+    'home'
 ]
 
 MIDDLEWARE = [
